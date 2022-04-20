b9a838af9c315d29a689708a4e56d455c67b6da0
UnderKo <hhit934@gmail.com>
2022-04-20 13:42:21 +0300

    D

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,5 +1,4 @@
 
-
 const WebSocketServer = require('ws');
 const wss = new WebSocketServer.Server({ port: 2332 })
 clients = []
@@ -7,15 +6,10 @@ wss.on("connection", ws => {
     console.log("new client connected");
     clients.push(ws);
     ws.on("message", event => {
-
-         
         var json_ = JSON.parse(event);
         var string_mes = json_.Name + ": " + json_.Message;
         console.log(string_mes);
         clients.forEach(client => client.send(JSON.stringify(json_ )));
-       
-        
-
     });
     ws.on("close", () => {
         console.log("the client has connected");
@@ -24,40 +18,4 @@ wss.on("connection", ws => {
         console.log("Some Error occurred")
     }
 });
-console.log("The WebSocket server is running on port 8080");
-
-
-
-
-
-
-
-
-
-// const WebSocket = require('ws');
-// const fs = require('fs');
-
-// const wsServer = new WebSocket.Server({ port: 2332 });
-// wsServer.on('connection', onConnect);
-
-// clients = []
-
- 
-
-// function onConnect(wsClient) {
-//     clients.push(wsClient);
-//     console.log('New user');
-//     wsClient.send('Hello');
-//     wsClient.on('message', function (message) {
-//          //console.log(JSON.stringify(message) );
-
-//         //var response = JSON.parse(message);
-//         console.log(message.data);
-      
-//     });
-//     wsClient.on('close', function () {
-//         console.log('User has been disconnected');
-//     });
-// }
-
-// console.log('Server has been started on port 2332 ');
+console.log("The WebSocket server is running.");
